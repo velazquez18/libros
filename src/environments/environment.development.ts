51e636b745334b9438ca8c5e15d50ba5955405a5
Diana Velázquez <dianapaolahvz@gmail.com>
2024-03-08 16:35:45 -0600

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyB2myIWCVkL6SNsCPrrjuF9EQB8-LYiC-4",
+        authDomain: "librosdd-fc966.firebaseapp.com",
+        projectId: "librosdd-fc966",
+        storageBucket: "librosdd-fc966.appspot.com",
+        messagingSenderId: "895639690607",
+        appId: "1:895639690607:web:a817197f614265c870dc9a"
+    }
+};
